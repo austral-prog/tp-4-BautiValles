7d355efc96cc14ddf572ffcd262eb23387245ed9
BautiValles <150041744+BautiValles@users.noreply.github.com>
2025-04-13 15:07:30 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,15 @@
 def leap_year():
-    print("TO DO")
+    año = float(input("Ingrese un año: "))
+    Div4 = año / 4
+    Div100 = año / 100
+    Div400 = año / 400
+    if ((Div4 - int(Div4) ) == 0.0):
+        if ((Div100 - int(Div100) ) == 0.0):
+            if ((Div400 - int(Div400) ) == 0.0):
+                print(f"El año {int(año)} es bisiesto")
+            else:
+                print(f"El año {int(año)} no es bisiesto")
+        else:
+            print(f"El año {int(año)} es bisiesto")
+    else:
+        print(f"El año {int(año)} no es bisiesto")
